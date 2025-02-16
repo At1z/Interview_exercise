a182361d735176a51f0bef4ffb21ec55a4869748
at1z <adrianlistkiewiczv2@gmail.com>
2025-02-16 11:43:46 +0100

    ?

--- remitly_project/src/test/java/com/atis/remitly_project/SwiftCodeControllerTest.java
+++ remitly_project/src/test/java/com/atis/remitly_project/SwiftCodeControllerTest.java
@@ -39,4 +39,33 @@ class SwiftCodeControllerTest {
                         "Swift code must be exactly 11 characters: 6 letters followed by 5 letters or numbers"
                 ));
     }
+    @Test
+    void getSwiftCode_ValidSwiftCode_ReturnsNotFound() throws Exception {
+        mockMvc.perform(get("/v1/swift-codes/AIZKLVAAXXX"))
+                .andExpect(status().isNotFound());
+    }
+
+    @Test
+    void getSwiftCode_ValidSwiftCode_isOk() throws Exception {
+        mockMvc.perform(get("/v1/swift-codes/ABIEBGS1XXX"))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    void getSwiftCode_InvalidSwiftCodeLowercase_ReturnsBadRequest() throws Exception {
+        mockMvc.perform(get("/v1/swift-codes/AIZKLV22XXx"))
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.message").value(
+                        "Swift code must be exactly 11 characters: 6 letters followed by 5 letters or numbers"
+                ));
+    }
+
+    @Test
+    void getSwiftCode_InvalidSwiftCodeNumberInFirstSix_ReturnsBadRequest() throws Exception {
+        mockMvc.perform(get("/v1/swift-codes/AI2KLV22XXX"))
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.message").value(
+                        "Swift code must be exactly 11 characters: 6 letters followed by 5 letters or numbers"
+                ));
+    }
 }
